996cccd24eb51864516f7cc1f1e074c930567e03
EringiShimeji <h5a2p9p6iruku@gmail.com>
2022-02-25 11:21:47 +0900

    init

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::{env, process};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("引数の個数が正しくありません");
+        process::exit(1);
+    }
+
+    println!(".intel_syntax noprefix");
+    println!(".globl main");
+    println!("main:");
+    println!("  mov rax, {}", args[1]);
+    println!("  ret");
+}
